b0d39417594329ca38df345b63517c536b12a008
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-02-04 13:05:50 +0200

    Create navbar  with mobile style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-
+import { useEffect, useState } from 'react'
+import NavBar from '@/Components/NavBar/NavBar';
+import { SelectedPage } from '@/shared/types';
 
 export const App = () => {
-	return (
-		<div className="app">
+	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY === 0) {
+				setIsTopOfPage(true);
+				setSelectedPage(SelectedPage.Home);
+			}
+			if (window.scrollY !== 0) {
+				setIsTopOfPage(false);
+			}
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, [])
+
+	return (
+		<div className="app bg-gray-20">
+			<NavBar
+				isTopOfPage={isTopOfPage}
+				selectedPage={selectedPage}
+				setSelectedPage={setSelectedPage}
+			/>
 		</div>
 	)
 };
